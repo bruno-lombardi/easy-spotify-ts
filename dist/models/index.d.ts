4a83301ee0fbce4a2aafc1b6ecb2dd06d77e195d
bruh-code <djfrizer1@gmail.com>
2018-11-27 21:19:44 -0300

    fixing for models dir on build

---
+++ dist/models/index.d.ts
@@ -0,0 +1,10 @@
+import { Album } from "./Album";
+import { Artist, SimplifiedArtist } from "./Artist";
+import { Copyright } from "./Copyright";
+import { ExternalIDS } from "./ExternalIDS";
+import { ExternalUrls } from "./ExternalUrls";
+import { Followers } from "./Followers";
+import { Image } from "./Image";
+import { Paging, PagingTracks } from "./Paging";
+import { SimplifiedTrack } from "./SimplifiedTrack";
+export { Album, Artist, SimplifiedArtist, Copyright, ExternalIDS, ExternalUrls, Followers, Image, Paging, PagingTracks, SimplifiedTrack, };
---
+++ dist/models/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Album_1 = require("./Album");
+exports.Album = Album_1.Album;
